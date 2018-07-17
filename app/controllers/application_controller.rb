53f8d1c20b0e5e5247e14545044bbdd3dbcf006e
Carrie Savinell <carrielane89@gmail.com>
2018-07-17 16:26:05 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
-    set :views, 'app/views/posts'
+    set :views, 'app/views'
   end
 
 #-----------CREATE --CRUD--
